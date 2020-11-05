ff6e6f60b8d8316bb0cffdcdb1d723b241337c6b
Madina-IP-18-3 <73819923+Madina-IP-18-3@users.noreply.github.com>
2020-11-05 12:12:58 +0300

    Add files via upload

---
+++ JMM_Task_03.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace JMM_Task_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string password = "aVd_12";
+              Console.WriteLine("Введите пароль ");
+            for (int i = 1; i <= 3; i++)
+            {                
+               string kod = Console.ReadLine();
+                if (password == kod)
+                   {
+                      Console.WriteLine("P.S. Скоро новый год!");
+                      break;
+                   }
+                else
+                   {
+                      Console.WriteLine(" Пароль неверный. Попытка {0}/3", i );
+                   }
+                                
+            };
+            
+            Console.ReadKey();
+
+        }
+    }
+}
