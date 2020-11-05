a646a9415f799c20e5f5094b25aa4c544133ef6b
Madina-IP-18-3 <73819923+Madina-IP-18-3@users.noreply.github.com>
2020-11-05 12:11:45 +0300

    Add files via upload

---
+++ JMM_Tack_02.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace JMM_Task_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = " ";
+            int sum = 0;
+
+                do
+                {
+
+                    word = Console.ReadLine();
+                    sum++;
+
+                }
+                while (word!= ("exit"));
+            Console.WriteLine("Количество введенных  слов " + sum);
+            Console.ReadKey();
+            }
+    }
+}
